f3bf871d60275b3fbbf88d75c433488c0ba25076
nguyentuan13021998 <nguyentuan13021998@gmail.com>
2021-07-09 17:53:56 +0700

    Add project files.

---
+++ my_eShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_eShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
